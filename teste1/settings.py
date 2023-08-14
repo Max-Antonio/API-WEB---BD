535dd9acc2ec3b3d134ffc77268913690ceefb0d
Max-Antonio <114777087+Max-Antonio@users.noreply.github.com>
2023-08-14 01:41:52 -0300

    Update settings.py

--- teste1/settings.py
+++ teste1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4lxr($cxt#0g*ocq7jrr10r=fiw92rm^_z=7q)eo$*xq(skis0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
